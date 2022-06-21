2cfcd342201cc9884d9f95fce24f03d439f23e80
clcondorcet <clement.rostagni@free.fr>
2022-06-21 09:08:57 +0200

    Adding root protection for mysql

--- config/mysql_conf.js
+++ config/mysql_conf.js
@@ -1,7 +1,7 @@
 module.exports = {
     connectionLimit: 20,
     user: 'root',
-    password: 'root',
+    password: '6rc/XaB*aqCd-V-b+@U~Q=xc^pyzXf',
     host: 'mysql',
     port: 3306,
     database: 'crhack'
